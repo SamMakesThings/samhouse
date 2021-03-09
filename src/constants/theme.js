25e1a1d313e3555037b21054f3f36fbd68568c55
Sam Stowers <phantom4d@gmail.com>
2021-03-08 23:48:49 -0600

    Add theme file & basic colors

---
+++ src/constants/theme.js
@@ -0,0 +1,10 @@
+export const clubTheme = {
+  test: 'test',
+  colors: {
+    highlight: '#27ae60',
+    warning: '#eb5757',
+    mainBg: '#f2efe4',
+    iconColor: '#000000',
+    h1: '#333333',
+  },
+};
