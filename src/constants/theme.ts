4f76932f8c8a24298400b32f2b2432b5d8138391
Sam Stowers <phantom4d@gmail.com>
2021-03-10 15:09:18 -0600

    Make theme file TS, add two more colors

--- src/constants/theme.ts
+++ src/constants/theme.ts
@@ -1,9 +1,10 @@
 export const theme = {
-  test: 'test',
   colors: {
     highlight: '#27ae60',
     warning: '#eb5757',
     mainBg: '#f2efe4',
+    subBg: '#e7e3d5',
+    cardBg: '#feffff',
     iconColor: '#000000',
     h1: '#333333',
   },
