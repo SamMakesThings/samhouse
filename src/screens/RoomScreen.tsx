cb4e14e7fb35cf9337afbfa57295c3a4342484c1
Sam Stowers <phantom4d@gmail.com>
2021-04-02 15:14:21 -0500

    Use ProfileThumbnail, add flex layout for thumbnls

--- src/screens/RoomScreen.tsx
+++ src/screens/RoomScreen.tsx
@@ -3,6 +3,7 @@ import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
 import { scale } from 'react-native-size-matters'
 import { theme } from '../constants/theme'
 import { ProfilePic } from '../components/ProfilePic'
+import { ProfileThumbnail } from '../components/ProfileThumbnail'
 
 // This component will go inside a Modal component in RoomBrowser.tsx
 export const RoomScreen = () => {
@@ -10,13 +11,26 @@ export const RoomScreen = () => {
     <View>
       <Text style={{...theme.text.h1, ...styles.Text_RoomTitle}}>Room title here, something something Clubhouse</Text>
       <View style={styles.View_People}>
-        <View style={styles.View_PersonThumbnail}>
-          <ProfilePic size={scale(60)}/>
-          <Text style={{...theme.text.h1, ...styles.Text_ProfilePic}}>Sam Stowers</Text>
-          <View style={styles.View_MuteIcon}>
-            <Text>ICON</Text>
-          </View>
-        </View>
+        <ProfileThumbnail
+          name={"Sam Stowers"}
+          muted={true}
+        />
+        <ProfileThumbnail
+          name={"Sam Stowers"}
+          muted={true}
+        />
+        <ProfileThumbnail
+          name={"Sam Stowers"}
+          muted={true}
+        />
+        <ProfileThumbnail
+          name={"Sam Stowers"}
+          muted={true}
+        />
+        <ProfileThumbnail
+          name={"Sam Stowers"}
+          muted={true}
+        />
       </View>
     </View>
   )
@@ -27,19 +41,9 @@ const styles = StyleSheet.create({
     fontSize: scale(15)
   },
   View_People: {
-    marginTop: scale(15)
-  },
-  View_PersonThumbnail: {
-
-  },
-  Text_ProfilePic: {
-    fontSize: scale(11.5),
-    letterSpacing: scale(-0.3),
-    marginTop: scale(5)
-  },
-  View_MuteIcon: {
-    marginTop: scale(-35),
-    backgroundColor: theme.colors.cardBg,
-    marginLeft: scale(35)
+    marginTop: scale(15),
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
   },
 })
