54053429cde11bb4e90bd37e130d215a4c7d685e
Sam Stowers <phantom4d@gmail.com>
2021-04-03 14:26:43 -0500

    Add actual mute icon to room screen

--- src/components/ProfileThumbnail.tsx
+++ src/components/ProfileThumbnail.tsx
@@ -3,6 +3,8 @@ import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
 import { scale } from 'react-native-size-matters'
 import { theme } from '../constants/theme'
 import { ProfilePic } from '../components/ProfilePic'
+import MuteIcon from '../assets/img/CHMute.svg'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
 // This component will go inside a Modal component in RoomBrowser.tsx
 export const ProfileThumbnail = (props: {name: string; muted: boolean;}) => {
@@ -11,7 +13,7 @@ export const ProfileThumbnail = (props: {name: string; muted: boolean;}) => {
       <ProfilePic size={scale(60)}/>
       <Text style={{...theme.text.h1, ...styles.Text_ProfilePic}}>{props.name}</Text>
       <View style={{...styles.View_MuteIcon, display: props.muted ? 'flex' : 'none'}}>
-        <Text>ICON</Text>
+        <MuteIcon width={scale(17)} height={scale(17)} />
       </View>
     </View>
   )
@@ -20,7 +22,6 @@ export const ProfileThumbnail = (props: {name: string; muted: boolean;}) => {
 const styles = StyleSheet.create({
   View_PersonThumbnail: {
     marginHorizontal: scale(10),
-    marginBottom: scale(35)
   },
   Text_ProfilePic: {
     fontSize: scale(11.5),
@@ -28,8 +29,13 @@ const styles = StyleSheet.create({
     marginTop: scale(5)
   },
   View_MuteIcon: {
-    marginTop: scale(-35),
+    marginTop: scale(-40),
+    marginBottom: scale(40),
     backgroundColor: theme.colors.cardBg,
-    marginLeft: scale(35)
+    marginLeft: scale(38),
+    borderRadius: scale(50),
+    padding: scale(3),
+    alignSelf: 'center',
+    elevation: scale(1)
   },
 })
